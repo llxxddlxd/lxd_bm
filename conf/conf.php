bfa622aa5dee53204e8854137f352792859e1bbc
llxxddlxd <llxxdd_lxd@126.com>
2018-08-15 18:10:00 +0800

    修改配置信息

--- conf/conf.php
+++ conf/conf.php
@@ -3,7 +3,8 @@
 $configure = [
 	"base"=>[ 
 		"testUrl"=>"http://seed1.bumotest.io:26002/",
-		"sourceAddress"=>"buQsurH1M4rjLkfjzkxR9KXJ6jSu2r9xBNEw",
+        "sourceAddress"=>"buQnv2Ym4mCUyd4L9cvVAvBYaPF2Levt4Sds", //手动在bumo测试环境中，生成的地址，内有200测试bu
+		"sourcePriKey"=>"privbz4GWB4kRbxHoZVGo5JY3AExg34338AT8s1f9aVXbZw3wC3dNDHB", //手动在bumo测试环境中，生成的地址，内有200测试bu
 	]
 ];
 return $configure;
